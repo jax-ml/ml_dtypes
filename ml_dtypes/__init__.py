6a59897b2e6467134d325336a7d8e74a5783481b
Jake VanderPlas <jakevdp@google.com>
2023-03-22 08:57:59 -0700

    Bump version to 0.0.3

--- ml_dtypes/__init__.py
+++ ml_dtypes/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.0.2'  # Keep in sync with pyproject.toml:version
+__version__ = '0.0.3'  # Keep in sync with pyproject.toml:version
 
 from typing import Type
 
