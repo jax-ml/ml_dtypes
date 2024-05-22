b13fd89b064b99a0afa52415a4a5b1826d119223
Peter Hawkins <phawkins@google.com>
2024-05-22 08:57:31 -0700

    [ml_dtypes] Remove unused int4 forwarding header.
    PiperOrigin-RevId: 636184058

--- ml_dtypes/include/int4.h
+++
@@ -1,8 +0,0 @@
-#ifndef THIRD_PARTY_PY_ML_DTYPES_INCLUDE_INT4_H_
-#define THIRD_PARTY_PY_ML_DTYPES_INCLUDE_INT4_H_
-
-// IWYU pragma: begin_exports
-#include "include/intn.h"  // INLINER_FORWARD_TO
-// IWYU pragma: end_exports
-
-#endif  // THIRD_PARTY_PY_ML_DTYPES_INCLUDE_INT4_H_
